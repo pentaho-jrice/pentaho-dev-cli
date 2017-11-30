f6539861673cd7895e2f698fcdd45932664185b2
Joe Rice <jrice@pentaho.com>
2017-11-30 05:44:02 +0000

    added sample dev config YML file

--- src/pentaho-ael.js
+++ src/pentaho-ael.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const config = require('./lib/config/default-config');
+const config = require('./lib/pcli/config/default-config');
 
 console.log("<<-- executed pentaho ael userHomeDir:  " + config.defaultConfig.userHomeDir);
 console.log("<<-- executed pentaho ael devConfigFilePath:  " + config.defaultConfig.devConfigFilePath);
+console.log("   os:  " + process.platform);
