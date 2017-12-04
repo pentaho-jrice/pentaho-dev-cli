bf018041f62de0cbd2fd4b4cdf0ddc21e6a54329
Joe Rice <jrice@pentaho.com>
2017-12-04 03:49:56 +0000

    Added something that looks close to final skeleton scaffolding.  Got prelim iniltialization functionality working

--- src/pentaho-ael.js
+++ src/pentaho-ael.js
@@ -1,7 +1,44 @@
 #!/usr/bin/env node
 
 const config = require('./lib/pcli/config/default-config');
+const cliContextBuilder = require('./lib/pcli/core/cli-context-builder');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
-console.log("<<-- executed pentaho ael userHomeDir:  " + config.defaultConfig.userHomeDir);
-console.log("<<-- executed pentaho ael devConfigFilePath:  " + config.defaultConfig.devConfigFilePath);
-console.log("   os:  " + process.platform);
+const cliContext = cliContextBuilder.initalizeCliContext();
+
+// try {
+//     var configFileDoc = yaml.safeLoad(fs.readFileSync(config.defaultConfig.devConfigFilePath, 'utf8'));
+//     console.log(configFileDoc);
+// } catch (e) {
+//     console.log(e);
+// }
+//
+// console.log("\n\n");
+//
+// try {
+//     var remoteBuildAppRuntimeCacheDoc = JSON.parse(fs.readFileSync(config.defaultConfig.remoteBuildAppRuntimeCacheFilePath, 'utf8'));
+//     console.log(JSON.stringify(remoteBuildAppRuntimeCacheDoc, null, 2));
+// } catch (e) {
+//     console.log(e);
+// }
+//
+// class CacheMeta {
+// }
+//
+// class RemoteBuildAppRuntime {
+// }
+//
+// let remoteBuildAppRuntime = new RemoteBuildAppRuntime();
+//
+// let cache = new CacheMeta();
+//
+// cache.lastRunTimeStamp = new Date();
+//
+// remoteBuildAppRuntime.cacheMeta = cache;
+//
+// console.log("\n\n");
+//
+// console.log(JSON.stringify(remoteBuildAppRuntime, null, 2));
+//
+// configFileDoc.joe;
